4cdf6289224612612c6f264cda8cc6601af8ef7c
BINODNAYAK806 <binodnayak8066@gmail.com>
2023-02-19 04:12:48 +0530

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+// import { ClassNames } from '@emotion/react';
+import React from 'react'
+import styles from "../Style/navbar.module.css";
+
+
+export default function Navbar() {
+  return (
+    
+    <div className={styles.main}    >
+        <div className={styles.ndiv}>
+            <img className={styles.img1} src="https://static.sadhguru.org/d/46272/1642843313-msr_logo.png" alt="" srcset="" />
+
+            <div className={styles.hut}>
+            <p>why mahashivrathri</p>
+            <p>mahashivrathri celebration</p>
+            <p>How to praticipate</p>
+            <p>siva </p>
+            
+
+            </div>
+            </div>
+
+
+    </div>
+  )
+}
